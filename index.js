360669ef3288ff7771599d82e9f8b8f4f7ed14ec
Kiruidyno <kirui472@gmail.com>
2023-03-15 18:08:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+var customerName='bob';
+
+function upperCaseCustomerName (){
+    var upperCaseCustomerName=customerName.toUpperCase();
+    customerName=upperCaseCustomerName;
+} 
+    
+
+function setBestCustomer(){
+    window.bestCustomer='not bob';
+}
+bestCustomer();
+
+
+
+
+function overwriteBestCustomer(newCustomer){
+    window.bestCustomer='maybe bob';
+}
+overwriteBestCustomer();
+
+const leastFavoriteCustomer= 'Manu';
+    function changeLeastFavoriteCustomer(){
+        leastFavoriteCustomer='Kyalo'
+
+    }
